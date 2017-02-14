b5e016c1a60365d5da36e29d7bd736b08a128a1b
Xue Bing <cheerbase@163.com>
2017-02-14 16:24:54 +0800

    code blue set up

---
+++ Source/CodeBlue/CodeBlue.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CodeBlue.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CodeBlue, "CodeBlue" );
---
+++ Source/CodeBlue/CodeBlueGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "CodeBlueGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CODEBLUE_API ACodeBlueGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
